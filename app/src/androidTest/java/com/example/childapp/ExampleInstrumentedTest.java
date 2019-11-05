6fa90db0c1a7ab9059df57b17772816f75ab11d6
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2019-11-04 19:22:25 -0800

    Adding @test methods

--- app/src/androidTest/java/com/example/childapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/childapp/ExampleInstrumentedTest.java
@@ -30,6 +30,25 @@ public class ExampleInstrumentedTest {
     public void addition() {
         assertEquals(4, 2 + 3);
        //// Log i()
+
+    }
+
+    @Test
+    public void matchCorrect() {
+        assertEquals("It's a match", 1, matchShape.getMatch);
+    }
+
+    @Test
+    public void buttonOne_initiates_game_matching_shapes() {
+
+        Game g = new Game(1);
+        assertEquals(1, g.getGameMode());
+
+    }
+
+    @Test
+    public void shape_added_to_list(){
+
     }
 
 
