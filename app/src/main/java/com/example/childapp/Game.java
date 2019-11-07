72931e19861038ff6f5714bcc4e43557a7480a4b
adamgerhartz <adamgerhartz93@byui.edu>
2019-11-06 21:51:59 -0500

    stub Game class.

--- app/src/main/java/com/example/childapp/Game.java
+++ app/src/main/java/com/example/childapp/Game.java
@@ -1,14 +1,52 @@
 package com.example.childapp;
 
-public class Game implements Runnable {
-    private int gameMode;
+import java.util.ArrayList;
+import java.util.List;
 
-    Game(int gameMode) { this.gameMode = gameMode; }
+public class Game {
+    private int _gameMode;
+    private List<Item> shapes;
 
-    public int getGameMode() { return gameMode; }
+    Game(int gameMode) {
+        _gameMode = gameMode;
+        shapes = new ArrayList<Item>();
+    }
 
-    @Override
+    // CREATE SHAPES
+    private void createShapes() {
+        // stub
+        // create instances of shapes
+        // append to array
+    }
+
+    // DELETE ARRAY
+    private void clearShapes() {
+        // stub
+        // question: clear the array or just update the indices?
+        // performance issue --- look up what's better
+    }
+
+    // add color
+    private void setColorsToShape() {
+        // stub
+        // handles color assignments
+    }
+
+    // DRIVER
     public void run() {
+        // stub
+        // driver
+        // create four shapes
+        // randomize color
+        // check is matches
+        // clears list if matched
+    }
 
+    private boolean matchShapes() {
+        // stub needs to know which game mode
+        // this handles the matching logic
+        // might not need to pass gamemode
+        //
+        return true;
     }
 }
