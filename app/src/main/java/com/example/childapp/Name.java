9b377cbf833d8aea9055d60a3d59f6304d34de0a
adamgerhartz <adamgerhartz93@byui.edu>
2019-11-07 20:28:51 -0500

    Delete Shape's Name enum

--- app/src/main/java/com/example/childapp/Name.java
+++
@@ -1,5 +0,0 @@
-package com.example.childapp;
-
-public enum Name {
-    Circle, Square, Triangle, Diamond, Star, Heart
-}
--- app/src/main/java/com/example/childapp/Shape.java
+++ app/src/main/java/com/example/childapp/Shape.java
@@ -2,13 +2,13 @@ package com.example.childapp;
 
 import android.graphics.Color;
 
-public abstract class Item {
+public abstract class Shape {
 
     // private member
     private ShapeName _shape;
     private String _color;
 
-    public Item(ShapeName shape, String color) {
+    public Shape(ShapeName shape, String color) {
         _shape = shape;
         _color = color;
     }
