f31902e6a0780e5e30cabbe16adfdc6b529359f7
adamgerhartz <adamgerhartz93@byui.edu>
2019-11-07 21:05:53 -0500

    Stub out new Ending Activity

---
+++ app/src/main/java/com/example/childapp/EndingScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.childapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ending_screen);
+    }
+}
