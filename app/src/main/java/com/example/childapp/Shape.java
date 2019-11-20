dcca3ce0ff758d2dadb64a7c4acf8ff76e65633f
adamgerhartz <adamgerhartz93@byui.edu>
2019-11-19 20:24:53 -0500

    Item back to Shape

--- app/src/main/java/com/example/childapp/Shape.java
+++ app/src/main/java/com/example/childapp/Shape.java
@@ -1,8 +1,42 @@
 package com.example.childapp;
 
-public interface Shape {
-    void draw();
-    Name getShapeName();
-    SelectedColor getShapeColor();
-    void changeShapeColor(SelectedColor color);
+//import android.graphics.Color;
+
+public abstract class Shape {
+
+    // private member
+    private Name _name;
+    private SelectedColor _color;
+    // private boolean _isMain;
+
+    public Shape(Name name, SelectedColor color/*, boolean isMain*/) {
+        _name = name;
+        _color = color;
+        // _isMain = isMain;
+    }
+
+    // pure virtual function
+    // abstract void draw();
+
+    // GETTERS
+    public Name getShape() {
+        return _name;
+    }
+
+    public SelectedColor getColor() {
+        return _color;
+    }
+
+    public void changeColor(SelectedColor color) { this._color = color; }
+
+    // SETTER
+
+    //abstract ShapeName getShape();
+
+    //abstract Color getColor();
+
+    // "Testing Adam Branch"
+
+
+
 }
