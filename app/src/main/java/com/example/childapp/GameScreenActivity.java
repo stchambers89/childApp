7996ccdf96e8c07f3d74e3e59072d74b5c6b0a3f
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2019-11-20 20:42:14 -0700

    Fixing touch and drag

--- app/src/main/java/com/example/childapp/GameScreenActivity.java
+++ app/src/main/java/com/example/childapp/GameScreenActivity.java
@@ -2,17 +2,18 @@ package com.example.childapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.solver.widgets.ConstraintTableLayout;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.gson.Gson;
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +29,7 @@ import java.util.Stack;
 public class GameScreenActivity extends AppCompatActivity {
 
     //Game game;
-    ConstraintLayout li;
+    LinearLayout li;
     private int gameMode;
 
     private static final String TAG = "GameScreenActivity";
@@ -36,6 +37,7 @@ public class GameScreenActivity extends AppCompatActivity {
     private ImageView _shape1;
     private ImageView _shape2;
     private ImageView _shape3;
+    private ImageView _mainShape;
 
     private ViewGroup mainLayout;
     private int xDelta;
@@ -47,7 +49,7 @@ public class GameScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
-        li = (ConstraintLayout) findViewById(R.id.gamescreen_background);
+        li = findViewById(R.id.gamescreen_background);
 
         Intent intent = getIntent();
         gameMode = intent.getIntExtra(MainActivity.GAME_MODE, -1);
@@ -57,56 +59,38 @@ public class GameScreenActivity extends AppCompatActivity {
             Log.e(TAG, "ERROR: GAME MODE SHOULD NEVER BE -1");
         }
 
-
         ShapeBuilder shapeBuilder = new ShapeBuilder(gameMode);
         Stack<List<Shape>> stackOfShapes = shapeBuilder.getStackofShapes();
-
-        // Added for touch & drop handling
-        //mainLayout = (ConstraintLayout) findViewById(R.id.gamescreen_background);
 
         _shape1 = (ImageView) findViewById(R.id.shape1);
         _shape2 = (ImageView) findViewById(R.id.shape2);
         _shape3 = (ImageView) findViewById(R.id.shape3);
 
-        _shape1.setOnTouchListener(onTouchListener());
-        _shape2.setOnTouchListener(onTouchListener());
-        _shape3.setOnTouchListener(onTouchListener());
+        _mainShape = (ImageView) findViewById(R.id.mainShape);
+
+        _shape1.setOnTouchListener(onTouchListener(_shape1));
+        _shape2.setOnTouchListener(onTouchListener(_shape2));
+        _shape3.setOnTouchListener(onTouchListener(_shape3));
+
+        _mainShape.setOnDragListener((v, event) -> {
+            final int action = event.getAction();
+
+            switch (action) {
+                case DragEvent.ACTION_DROP:
+                    return true;
+                default:
+                    return false;
+            }
+        });
+
     }
 
-    private View.OnTouchListener onTouchListener() {
+    private View.OnTouchListener onTouchListener(ImageView img) {
         return (v, event) -> {
-            final int x = (int) event.getRawX();
-            final int y = (int) event.getRawY();
-
-
-            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-
-                case MotionEvent.ACTION_DOWN:
-                    ConstraintLayout.LayoutParams lParams = (ConstraintLayout.LayoutParams)
-                            v.getLayoutParams();
-
-                    xDelta = x - lParams.leftMargin;
-                    yDelta = y - lParams.topMargin;
-                    break;
-
-                case MotionEvent.ACTION_UP:
-                    Toast.makeText(GameScreenActivity.this,
-                            "Click and drag worked!", Toast.LENGTH_SHORT)
-                            .show();
-                    break;
-
-                case MotionEvent.ACTION_MOVE:
-                    ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) v
-                            .getLayoutParams();
-                    layoutParams.leftMargin = x - xDelta;
-                    layoutParams.topMargin = y - yDelta;
-                    layoutParams.rightMargin = 0;
-                    layoutParams.bottomMargin = 0;
-                    v.setLayoutParams(layoutParams);
-                    break;
-            }
-            li.invalidate();
-            return true;
+            ClipData data = ClipData.newPlainText("","");
+            View.DragShadowBuilder shadow = new View.DragShadowBuilder(img);
+            v.startDrag(data,shadow, null,0);
+            return false;
         };
     }
 
