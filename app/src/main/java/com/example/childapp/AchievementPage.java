0b49a034d1e55cc729f2df1988673fcacb8254e2
adamgerhartz <adamgerhartz93@byui.edu>
2019-11-26 22:30:49 -0500

    Javadocs to Achievement page

--- app/src/main/java/com/example/childapp/AchievementPage.java
+++ app/src/main/java/com/example/childapp/AchievementPage.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
+/**
+ * Achievement page class views a pop up that displays the achievements
+ *
+ */
 public class AchievementPage extends AppCompatActivity {
 
     @Override
@@ -14,15 +18,17 @@ public class AchievementPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement_page);
 
+        // alter window dimensions to display popup
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
+        // scale
         getWindow().setLayout((int) (width * 0.9), (int) (height * 0.85));
 
-        // get trophies
+        // get trophies from SharedPreferences
         SharedPreferences sharedPref = getSharedPreferences("achievement_file", MODE_PRIVATE);
         int numOfTrophies = sharedPref.getInt("Trophy Count", 0);
 
