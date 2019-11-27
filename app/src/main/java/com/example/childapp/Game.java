15b395ce0ec4b7f53bf7edf79fbb48f0f32a2028
stchambers89 <56280693+stchambers89@users.noreply.github.com>
2019-11-26 19:31:35 -0800

    cleaned up game class

--- app/src/main/java/com/example/childapp/Game.java
+++ app/src/main/java/com/example/childapp/Game.java
@@ -17,46 +17,4 @@ public class Game {
         _shapes = shapes;
     }
 
-    // Getter for our game mode
-    public int get_gameMode() { return _gameMode; }
-    // Getter for our list of shapes
-    public List<Shape> getShapes() { return _shapes; }
-
-    // CREATE SHAPES
-    private void createShapes() {
-        // stub
-        // create instances of shapes
-        // append to array
-    }
-
-    // DELETE ARRAY
-    private void clearShapes() {
-        // stub
-        // question: clear the array or just update the indices?
-        // performance issue --- look up what's better
-    }
-
-    // add color
-    private void setColorsToShape() {
-        // stub
-        // handles color assignments
-    }
-
-    // DRIVER
-    public void run() {
-        // stub
-        // driver
-        // create four shapes
-        // randomize color
-        // check is matches
-        // clears list if matched
-    }
-
-    private boolean matchShapes() {
-        // stub needs to know which game mode
-        // this handles the matching logic
-        // might not need to pass gamemode
-        //
-        return true;
-    }
 }
